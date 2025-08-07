352bf567191f053126ccff60919c47f65a5e380b
UmairSaeedBhatti <us5014302@gmail.com>
2025-08-07 17:23:34 +0200

    feat: add Messages component with ComingSoon display for direct messaging feature

---
+++ src/pages/Messages.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ComingSoon from "../components/common/ComingSoon";
+
+const Messages = () => {
+  return (
+    <ComingSoon
+      title="Messages"
+      description="Direct messaging feature is coming soon!"
+      icon="💬"
+    />
+  );
+};
+
+export default Messages;
